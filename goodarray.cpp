2d85989a005ddbd8bda0a49997acbb143103b90b
Reza Farhang <rezafarhangoa@gmail.com>
2020-03-29 01:09:18 +0430

    Good Array Problem Codeforces Round 521 With Cpp

---
+++ goodarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+int arrsize, arr[200000];
+cin>>arrsize;
+map<int,int> freq;
+int sum = 0, x;
+for(int i = 0; i < arrsize; i++){
+	cin>>x;
+	arr[i] = x;
+	sum += x;
+	freq[x]++;
+}
+int res[200000], cnt=0, s;
+for(int i = 0 ; i < arrsize; i++){
+	s = sum - arr[i];
+	
+	if(s % 2 ==0){
+		s = s >> 1;
+		if(freq[s]!=0)
+		    if((arr[i] == s && freq[s] > 1) || arr[i] != s)
+			        res[cnt++] = i + 1;
+		    
+	}
+}
+cout<<cnt<<endl;
+for(int i = 0; i < cnt; i++)
+	cout<<res[i]<<" ";
+
+}
