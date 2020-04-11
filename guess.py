70b48d9bf1feba15b7d17f895a703f65a45f600f
Reza Farhang <rezafarhangoa@gmail.com>
2020-04-11 14:01:24 +0430

    Guess  Problem Kattis With Python

---
+++ guess.py
@@ -0,0 +1,25 @@
+import math
+
+low = 1
+high = 1000
+turns = 10
+
+while turns > 0:
+    guess = int((high - low)/ 2 + low)
+    # print(low, high)
+    print(guess)
+    guess_ans = input()
+    if guess_ans == "lower":
+        high = guess - 1
+        if low > high:
+            low, high = high, low
+        turns -= 1
+    elif guess_ans == "higher":
+        low = guess + 1
+        if low > high:
+            low, high = high, low
+        turns -= 1
+    else:
+        turns = 0
+
+
